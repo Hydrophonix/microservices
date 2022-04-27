e52c2a444e082b1f54c27982ca0854e5b1abfedc
Hydrophonix <hydrophonixfx@gmail.com>
2022-04-27 10:21:48 +0300

    Updates

--- common/src/decorators/current-user.decorator.ts
+++ common/src/decorators/current-user.decorator.ts
@@ -1,9 +1,12 @@
 // Core
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
+// Instruments
+import { RequestWithUser } from "../interfaces";
+
 export const CurrentUser = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
-        const request = ctx.switchToHttp().getRequest();
+        const request = ctx.switchToHttp().getRequest<RequestWithUser>();
 
         return request.user;
     },
