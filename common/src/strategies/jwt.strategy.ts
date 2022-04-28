efdc2aa2f6d03cf99cdfe8906b85a3053f9e822a
Hydrophonix <hydrophonixfx@gmail.com>
2022-04-28 13:36:03 +0300

    Updates

--- common/src/strategies/jwt.strategy.ts
+++ common/src/strategies/jwt.strategy.ts
@@ -9,15 +9,13 @@ import { TokenPayload } from "../interfaces";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(
-        private readonly configService: ConfigService,
-    ) {
+    constructor() {
         super({
             jwtFromRequest: ExtractJwt.fromExtractors(
                 [ (request) => request?.cookies?.Authentication ],
             ),
             ignoreExpiration: false,
-            secretOrKey:      configService.get("JWT_SECRET"),
+            secretOrKey:      process.env.JWT_SECRET,
         });
     }
 
