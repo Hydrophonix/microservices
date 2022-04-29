f8576346f73db27cb76a8cbd85c2e0e2cde4ea68
Hydrophonix <hydrophonixfx@gmail.com>
2022-04-29 10:18:20 +0300

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -2,4 +2,8 @@ export enum Subjects {
     UserCreated = "user:created",
     UserUpdated = "user:updated",
     UserDeleted = "user:deleted",
+
+    PostCreated = "post:created",
+    PostUpdated = "post:updated",
+    PostDeleted = "post:deleted",
 }
